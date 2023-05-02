ca60f00df739153038e345f4639b1150aa8b9cf1
Tushar-ShahidurRahman <ptushar004@gmail.com>
2023-05-02 10:53:04 +0600

    theme data for elevated button added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'ui_presentation_layer/screens/splash_screen.dart';
+import 'ui_presentation_layer/utils/app_color.dart';
 
 void main() {
   runApp(const CraftyBay());
@@ -12,9 +13,35 @@ class CraftyBay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       title: 'An ecommerce app',
-      home: SplashScreen(),
+      home: const SplashScreen(),
+      themeMode: ThemeMode.light,
+      // Todo: Add theme toggle function between light and dark mode.
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.purple,
+          textStyle: const TextStyle(
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.6,
+          ),
+        ),
+      ),
+        // textTheme: TextTheme
+      ),
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: primaryColor,
+            textStyle: const TextStyle(
+              fontWeight: FontWeight.w400,
+              letterSpacing: 0.6,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
